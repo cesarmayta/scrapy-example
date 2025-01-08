ea185fca2b3f35186a6c6a5e67f822e5417ae6fa
César Mayta Avalos <cesarmayta@gmail.com>
2025-01-08 14:43:14 -0500

    example1

---
+++ spider_tutorial/spider_tutorial/spiders/worldometers.py
@@ -0,0 +1,21 @@
+import scrapy
+
+
+class WorldometersSpider(scrapy.Spider):
+    name = "worldometers"
+    allowed_domains = ["www.worldometers.info"]
+    start_urls = ["https://www.worldometers.info/world-population/population-by-country"]
+
+    def parse(self, response):
+        # simple extraction
+        title = response.xpath('//h1/text()').get()
+        # example 1 - basic
+        countries = response.xpath('//td/a/text()').getall()
+        yield {
+            'titles': title,
+            'countries': countries,
+        }
+       
+        
+        
+        
